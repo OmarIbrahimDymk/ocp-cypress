cea19448b3e25f147a25c919c7390571bf5cf5a5
omar ibrahim <omar.ibrahim@dynamiktechnologies.com.bn>
2021-04-27 09:44:56 +0800

    ✅add demo test

---
+++ cypress/integration/demo/Sum.spec.js
@@ -0,0 +1,15 @@
+let sum = (...args) => args.reduce((total, arg) => total + arg);
+
+xcontext("Sum function", () => {
+  it("should add two arguments", () => {
+    expect(sum(1, 1)).to.equal(2);
+  });
+
+  it("should add three arguments", () => {
+    expect(sum(1, 1, 1)).to.equal(3);
+  });
+
+  it("should add multiple arguments", () => {
+    expect(sum(1, 1, 1, 1, 1)).to.equal(5);
+  });
+});
